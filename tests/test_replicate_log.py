f35f626e92a248ec32a955f1a8466f0eb4f8cef5
MBrouns <matthijs.brouns@gmail.com>
2019-12-18 12:00:11 -0600

    refactor test for log replication

--- tests/test_replicate_log.py
+++ tests/test_replicate_log.py
@@ -4,6 +4,8 @@ from raft.server import LogAlreadyUpToDateException
 
 
 def replicate(leader, follower):
+    """Mocks the messages transferring between leaders and followers to do a full cycle of log replication"""
+
     leader_response_msg = Message(
         server_no=leader.server_no,
         term=leader.term,
@@ -28,14 +30,22 @@ def replicate(leader, follower):
         )
 
 
+def logs_same(log1, log2):
+    if len(log1) != len(log2):
+        return False
+    for i in range(len(log2)):
+        if log1[i] != log2[i]:
+            return False
+    return True
+
+
 def test_replicate_to_empty_log(
     no_network_raft_leader_with_log, no_network_raft_follower
 ):
+    """Tests whether log replication works when follower log is empty"""
     replicate(no_network_raft_leader_with_log, no_network_raft_follower)
 
-    assert len(no_network_raft_leader_with_log.log) == len(no_network_raft_follower.log)
-    for i in range(len(no_network_raft_follower.log)):
-        assert no_network_raft_leader_with_log.log[i] == no_network_raft_follower.log[i]
+    assert logs_same(no_network_raft_leader_with_log.log, no_network_raft_follower.log)
 
 
 def test_replicate_to_complete_log(
@@ -44,9 +54,7 @@ def test_replicate_to_complete_log(
     no_network_raft_follower.log._log = list(no_network_raft_leader_with_log.log._log)
     replicate(no_network_raft_leader_with_log, no_network_raft_follower)
 
-    assert len(no_network_raft_leader_with_log.log) == len(no_network_raft_follower.log)
-    for i in range(len(no_network_raft_follower.log)):
-        assert no_network_raft_leader_with_log.log[i] == no_network_raft_follower.log[i]
+    assert logs_same(no_network_raft_leader_with_log.log, no_network_raft_follower.log)
 
 
 def test_replicate_to_partial_log(
@@ -60,24 +68,32 @@ def test_replicate_to_partial_log(
 
     replicate(no_network_raft_leader_with_log, no_network_raft_follower)
 
-    assert len(no_network_raft_leader_with_log.log) == len(no_network_raft_follower.log)
-    for i in range(len(no_network_raft_follower.log)):
-        assert no_network_raft_leader_with_log.log[i] == no_network_raft_follower.log[i]
+    assert logs_same(no_network_raft_leader_with_log.log, no_network_raft_follower.log)
 
 
 def test_replicate_to_diverging_log(
     no_network_raft_leader_with_log, no_network_raft_follower
 ):
     """Tests whether log replication recovers after a split brain"""
-    no_network_raft_follower.log.append(log_index=0, prev_log_term=0, entry=LogEntry(term=0, msg=0))
-    no_network_raft_follower.log.append(log_index=1, prev_log_term=0, entry=LogEntry(term=0, msg=1))
-    no_network_raft_follower.log.append(log_index=2, prev_log_term=0, entry=LogEntry(term=0, msg=2))
-    no_network_raft_follower.log.append(log_index=3, prev_log_term=0, entry=LogEntry(term=0, msg=3))
-    no_network_raft_follower.log.append(log_index=4, prev_log_term=0, entry=LogEntry(term=0, msg=4))
-    no_network_raft_follower.log.append(log_index=5, prev_log_term=0, entry=LogEntry(term=0, msg=5))
+    no_network_raft_follower.log.append(
+        log_index=0, prev_log_term=0, entry=LogEntry(term=0, msg=0)
+    )
+    no_network_raft_follower.log.append(
+        log_index=1, prev_log_term=0, entry=LogEntry(term=0, msg=1)
+    )
+    no_network_raft_follower.log.append(
+        log_index=2, prev_log_term=0, entry=LogEntry(term=0, msg=2)
+    )
+    no_network_raft_follower.log.append(
+        log_index=3, prev_log_term=0, entry=LogEntry(term=0, msg=3)
+    )
+    no_network_raft_follower.log.append(
+        log_index=4, prev_log_term=0, entry=LogEntry(term=0, msg=4)
+    )
+    no_network_raft_follower.log.append(
+        log_index=5, prev_log_term=0, entry=LogEntry(term=0, msg=5)
+    )
 
     replicate(no_network_raft_leader_with_log, no_network_raft_follower)
 
-    assert len(no_network_raft_leader_with_log.log) == len(no_network_raft_follower.log)
-    for i in range(len(no_network_raft_follower.log)):
-        assert no_network_raft_leader_with_log.log[i] == no_network_raft_follower.log[i]
+    assert logs_same(no_network_raft_leader_with_log.log, no_network_raft_follower.log)
