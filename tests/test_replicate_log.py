6eb801b002e77b98402dcd752dfd7743ae3d6778
MBrouns <matthijs.brouns@gmail.com>
2019-12-18 11:57:00 -0600

    add test to cover split brain recovery in log replication

--- tests/test_replicate_log.py
+++ tests/test_replicate_log.py
@@ -1,4 +1,5 @@
-from raft.messaging import Message, AppendEntriesFailed
+from raft.log import LogEntry
+from raft.messaging import Message
 from raft.server import LogAlreadyUpToDateException
 
 
@@ -51,10 +52,29 @@ def test_replicate_to_complete_log(
 def test_replicate_to_partial_log(
     no_network_raft_leader_with_log, no_network_raft_follower
 ):
+    """Test whether log replication succesfully completes when starting with a partial log"""
     no_network_raft_follower.log._log = [
         no_network_raft_leader_with_log.log[0],
         no_network_raft_leader_with_log.log[1],
     ]
+
+    replicate(no_network_raft_leader_with_log, no_network_raft_follower)
+
+    assert len(no_network_raft_leader_with_log.log) == len(no_network_raft_follower.log)
+    for i in range(len(no_network_raft_follower.log)):
+        assert no_network_raft_leader_with_log.log[i] == no_network_raft_follower.log[i]
+
+
+def test_replicate_to_diverging_log(
+    no_network_raft_leader_with_log, no_network_raft_follower
+):
+    """Tests whether log replication recovers after a split brain"""
+    no_network_raft_follower.log.append(log_index=0, prev_log_term=0, entry=LogEntry(term=0, msg=0))
+    no_network_raft_follower.log.append(log_index=1, prev_log_term=0, entry=LogEntry(term=0, msg=1))
+    no_network_raft_follower.log.append(log_index=2, prev_log_term=0, entry=LogEntry(term=0, msg=2))
+    no_network_raft_follower.log.append(log_index=3, prev_log_term=0, entry=LogEntry(term=0, msg=3))
+    no_network_raft_follower.log.append(log_index=4, prev_log_term=0, entry=LogEntry(term=0, msg=4))
+    no_network_raft_follower.log.append(log_index=5, prev_log_term=0, entry=LogEntry(term=0, msg=5))
 
     replicate(no_network_raft_leader_with_log, no_network_raft_follower)
 
